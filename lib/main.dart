1c81e0b120dd9f86b6112239efa21cf09ceb542d
NadeebaAtiqui <nadeebaatiqui@gmail.com>
2023-09-25 23:54:54 -0500

    fully set up firebase with flutter project

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:myapp/firebase_options.dart';
 import 'package:myapp/pages/custom_widgets.dart';
 import 'pages/login.dart';
 
-void main() {
+Future <void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
